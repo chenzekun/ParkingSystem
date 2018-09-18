4a82c7b1d57df27aa2cc9689d443fb683f3554ba
lenovo <lenovo@LAPTOP-3G2LAQTB>
2018-09-18 17:24:36 +0800

    世埔 entity 1.0

---
+++ ParkingSystem/src/org/great/entity/AdminBean.java
@@ -0,0 +1,80 @@
+package org.great.entity;
+
+public class AdminBean {
+	private String adminId; //后台用户id
+	private String adminAccount;//账户名
+	private String adminPwd;//密码
+	private String adminName;//姓名
+	private String adminSex;//性别
+	private String adminTel;//电话
+	private String adminAddr;//住址
+	private String adminIDCard;//身份证
+	private String adminState;//后台用户状态
+	private String adminRegTime;//注册时间
+	public AdminBean() {
+		
+	}
+	
+	public String getAdminId() {
+		return adminId;
+	}
+	public void setAdminId(String adminId) {
+		this.adminId = adminId;
+	}
+	public String getAdminAccount() {
+		return adminAccount;
+	}
+	public void setAdminAccount(String adminAccount) {
+		this.adminAccount = adminAccount;
+	}
+	public String getAdminPwd() {
+		return adminPwd;
+	}
+	public void setAdminPwd(String adminPwd) {
+		this.adminPwd = adminPwd;
+	}
+	public String getAdminName() {
+		return adminName;
+	}
+	public void setAdminName(String adminName) {
+		this.adminName = adminName;
+	}
+	public String getAdminSex() {
+		return adminSex;
+	}
+	public void setAdminSex(String adminSex) {
+		this.adminSex = adminSex;
+	}
+	public String getAdminTel() {
+		return adminTel;
+	}
+	public void setAdminTel(String adminTel) {
+		this.adminTel = adminTel;
+	}
+	public String getAdminAddr() {
+		return adminAddr;
+	}
+	public void setAdminAddr(String adminAddr) {
+		this.adminAddr = adminAddr;
+	}
+	public String getAdminIDCard() {
+		return adminIDCard;
+	}
+	public void setAdminIDCard(String adminIDCard) {
+		this.adminIDCard = adminIDCard;
+	}
+	public String getAdminState() {
+		return adminState;
+	}
+	public void setAdminState(String adminState) {
+		this.adminState = adminState;
+	}
+	public String getAdminRegTime() {
+		return adminRegTime;
+	}
+	public void setAdminRegTime(String adminRegTime) {
+		this.adminRegTime = adminRegTime;
+	}
+	
+	
+}
---
+++ ParkingSystem/src/org/great/entity/PermissionBean.java
@@ -0,0 +1,22 @@
+package org.great.entity;
+
+public class PermissionBean {
+	private String perId; //权限id
+	private String perName;//权限名
+	public PermissionBean() {
+		
+	}
+	public String getPerId() {
+		return perId;
+	}
+	public void setPerId(String perId) {
+		this.perId = perId;
+	}
+	public String getPerName() {
+		return perName;
+	}
+	public void setPerName(String perName) {
+		this.perName = perName;
+	}
+	
+}
